600d7287fb44180c9a4fea44d747236af5800462
Lazjessicah <166005469+LJ-vdZ@users.noreply.github.com>
2025-06-19 19:33:27 +0200

    Scene Change Script
    - added ChangeScene script for the buttons (all the buttons are working)
    - just need to add a pause button in the Level 1 scene to go to the Pause scene

---
+++ DreamyMosaics/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private static string previousScene = null;
+
+    //helps go to previous scene, for the back buttons
+    private void LoadAndStorePreviousScene(string newSceneName)
+    {
+        previousScene = SceneManager.GetActiveScene().name;
+
+        SceneManager.LoadScene(newSceneName);
+    }
+
+    public void StartButton() //goes to Level 1 scene
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void AboutButton() //goes to Information Interface scene
+    {
+        LoadAndStorePreviousScene("Information Interface");
+    }
+
+    public void BackButton()    //goes back to previous scene
+    {
+        if (!string.IsNullOrEmpty(previousScene))
+        {
+            SceneManager.LoadScene(previousScene);
+        }
+        else
+        {
+            Debug.Log("No previous scene stored");
+        }
+    }
+
+    public void HomeButton() //goes to Game Start scene
+    {
+        SceneManager.LoadScene("Game Start");
+    }
+
+
+    public void RestartButton() //goes to Level 1 scene again, reload
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void NewGameButton() //also goes to Level 1 scene again, since we only have one level
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitButton() //quits application
+    {
+        Application.Quit();
+    }
+
+    public void MuteSoundButton() //mutes game sound
+    {
+
+    }
+
+    public void ShowScoreButton() //displays score
+    {
+        LoadAndStorePreviousScene("ScoreScene");
+    }
+
+
+}
