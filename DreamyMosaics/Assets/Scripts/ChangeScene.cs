9897ef61ef1100a082007b1bc0fadd89a3cc968a
Lazjessicah <166005469+LJ-vdZ@users.noreply.github.com>
2025-06-20 13:39:47 +0200

    Pause Menu & Buttons
    - deleted Pause Menu Scene and created Pause Menu overlay in the game scene
    - added home button and quit button to game scene

--- DreamyMosaics/Assets/Scripts/ChangeScene.cs
+++ DreamyMosaics/Assets/Scripts/ChangeScene.cs
@@ -35,37 +35,20 @@ public class ChangeScene : MonoBehaviour
         }
     }
 
-    public void ResumeButton()    //goes back to previous scene/game scene
-    {
-        if (!string.IsNullOrEmpty(previousScene))
-        {
-            SceneManager.LoadScene(previousScene);
-        }
-        else
-        {
-            Debug.Log("No previous scene stored");
-        }
-    }
-
     public void HomeButton() //goes to Game Start scene
     {
         SceneManager.LoadScene("Game Start");
     }
 
 
-    public void RestartButton() //goes to Level 1 scene again, reload
+    public void RestartButton() //goes to Level 1 scene, reload
     {
         SceneManager.LoadScene("Level 1");
     }
 
-    public void NewGameButton() //also goes to Level 1 scene again, since we only have one level
+    public void NewGameButton() //also goes to Level 1 scene, since we only have one level at the moment
     {
         SceneManager.LoadScene("Level 1");
-    }
-
-    public void PauseButton() //goes to pause scene
-    {
-        LoadAndStorePreviousScene("Pause Interface");
     }
 
     public void QuitButton() //quits application
